d26a6e8b4de7c6fbffead7a559f013158b4d6fc0
miloyip@gmail.com <miloyip@gmail.com@c5894555-1306-4e8d-425f-1f6f381ee07c>
2011-11-21 18:54:22 +0000

    Added missing Flush() in examples. Flush() will be called in Writer later.
    git-svn-id: https://rapidjson.googlecode.com/svn/trunk@25 c5894555-1306-4e8d-425f-1f6f381ee07c

--- example/condense/condense.cpp
+++ example/condense/condense.cpp
@@ -27,5 +27,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	os.Flush();
+
 	return 0;
 }
--- example/pretty/pretty.cpp
+++ example/pretty/pretty.cpp
@@ -24,5 +24,6 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	os.Flush();
 	return 0;
 }
