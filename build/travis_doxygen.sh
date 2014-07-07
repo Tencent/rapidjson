4a49c5eff6f0642a5ab58c5446cd7b525c153194
Milo Yip <miloyip@gmail.com>
2014-07-08 00:20:10 +0800

    build doxygen

---
+++ build/travis_doxygen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if ["${config}"="release64" && "$CC"="clang"] then exit 0 fi
+
+cd /tmp
+git clone https://github.com/doxygen/doxygen.git
+cd doxygen
+./configure
+make
+make distclean
+git pull
+./configure
+make
+make install
+
+cd "${TRAVIS_BUILD_DIR}
+doxygen build/Doxyfile
