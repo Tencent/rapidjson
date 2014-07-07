4720d4a2dd5ce1759c9e953aa7b731561a8b8945
Milo Yip <miloyip@gmail.com>
2014-07-08 01:18:38 +0800

    Change line ending

--- build/travis_doxygen.sh
+++ build/travis_doxygen.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-if ["${config}"="release64" && "$CC"="clang"] then exit 0 fi
 
-cd /tmp
-git clone https://github.com/doxygen/doxygen.git
-cd doxygen
-./configure
-make
-make distclean
-git pull
-./configure
-make
-sudo make install
+if [ "${config}" == "release64" ] && [ "$CC" == "clang" ]; then
+	cd /tmp
+	git clone https://github.com/doxygen/doxygen.git
+	cd doxygen
+	./configure
+	make
+	make distclean
+	git pull
+	./configure
+	make
+	sudo make install
 
-cd "${TRAVIS_BUILD_DIR}"
-doxygen build/Doxyfile
+	cd "${TRAVIS_BUILD_DIR}"
+	doxygen build/Doxyfile
+fi
