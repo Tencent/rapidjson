025c516f827845c2510f761b203fce7ae3b669a1
Milo Yip <miloyip@gmail.com>
2014-07-13 01:00:03 +0800

    Add kParseErrorTermination English error message.

--- include/rapidjson/error/en.h
+++ include/rapidjson/error/en.h
@@ -38,6 +38,8 @@ inline const RAPIDJSON_ERROR_CHARTYPE* GetParseError_En(ParseErrorCode parseErro
 		case kParseErrorNumberMissFraction:				return RAPIDJSON_ERROR_STRING("Miss fraction part in number.");
 		case kParseErrorNumberMissExponent:				return RAPIDJSON_ERROR_STRING("Miss exponent in number.");
 
+		case kParseErrorTermination:					return RAPIDJSON_ERROR_STRING("Parsing was terminated.");
+
 		default:
 			return RAPIDJSON_ERROR_STRING("Unknown error.");
 	}
