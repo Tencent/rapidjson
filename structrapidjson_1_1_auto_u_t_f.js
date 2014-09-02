b1c2ffcbae195b834f51e301e61e3e66926e000f
miloyip <miloyip@gmail.com>
2014-09-02 13:56:31 +0000

    Automatic doxygen build

--- structrapidjson_1_1_auto_u_t_f.js
+++ structrapidjson_1_1_auto_u_t_f.js
@@ -1,5 +1,5 @@
 var structrapidjson_1_1_auto_u_t_f =
 [
     [ "Ch", "structrapidjson_1_1_auto_u_t_f.html#a8ba58f529fad9b33dc419b12ee13844d", null ],
-    [ "supportUnicode", "structrapidjson_1_1_auto_u_t_f.html#afa6353f806b1b1e39735bac33c2d61a0a15b55b712f9e34d146fb236e5a89e06e", null ]
+    [ "supportUnicode", "structrapidjson_1_1_auto_u_t_f.html#a97917924016223755d1f2f70812ea6eda15b55b712f9e34d146fb236e5a89e06e", null ]
 ];
--- structrapidjson_1_1_u_t_f8.js
+++ structrapidjson_1_1_u_t_f8.js
@@ -1,5 +1,5 @@
 var structrapidjson_1_1_u_t_f8 =
 [
     [ "Ch", "structrapidjson_1_1_u_t_f8.html#a9f47e775d8306a647a5c9eceac4b52fc", null ],
-    [ "supportUnicode", "structrapidjson_1_1_u_t_f8.html#afce682450148679f2fe55dd076a18b0da821cba43661b67d78115cc9366e91b32", null ]
+    [ "supportUnicode", "structrapidjson_1_1_u_t_f8.html#a2855bd8436afb593c19d8746e189c895a821cba43661b67d78115cc9366e91b32", null ]
 ];
