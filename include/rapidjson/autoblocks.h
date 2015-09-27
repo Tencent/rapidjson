b8b1a44d40776d45c880e31e3d0a82afbdc4d240
Bruno Nicoletti <bruno.j.nicoletti@googlemail.com>
2015-09-27 11:50:53 +0100

    Fixed copyright on the files I changed

--- include/rapidjson/autoblocks.h
+++ include/rapidjson/autoblocks.h
@@ -1,4 +1,6 @@
-// Copyright (C) 2015 Bruno Nicoletti
+// Tencent is pleased to support the open source community by making RapidJSON available.
+//
+// Copyright (C) 2015 THL A29 Limited, a Tencent company, Milo Yip and Bruno Nicoletti. All rights reserved.
 //
 // Licensed under the MIT License (the "License"); you may not use this file except
 // in compliance with the License. You may obtain a copy of the License at
--- include/rapidjson/writerbase.h
+++ include/rapidjson/writerbase.h
@@ -1,8 +1,6 @@
 // Tencent is pleased to support the open source community by making RapidJSON available.
 //
-// Copyright (C) 2015 THL A29 Limited, a Tencent company, and Milo Yip. All rights reserved.
-//
-// Additional modifcations, Copyright (C) 2015, Bruno Nicoletti. All rights reserved.
+// Copyright (C) 2015 THL A29 Limited, a Tencent company, Milo Yip and Bruno Nicoletti. All rights reserved.
 //
 // Licensed under the MIT License (the "License"); you may not use this file except
 // in compliance with the License. You may obtain a copy of the License at
