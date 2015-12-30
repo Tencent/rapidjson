db11011a5c12e4efa0e27a915737f5af4765454c
Omer Katz <omer.drow@gmail.com>
2015-12-30 15:55:24 +0200

    Script seems to fail to report coverage. Explictly specifying the commands.

--- travis-coveralls.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install --user cpp-coveralls
-coveralls -r .. --gcov-options '\-lp' -e thirdparty -e example -e test -e build/CMakeFiles -e include/rapidjson/msinttypes -e include/rapidjson/internal/meta.h -e include/rapidjson/error/en.h
