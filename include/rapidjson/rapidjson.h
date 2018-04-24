73b8774ab16c6474c4759853059fdfdf7559b5c9
Stephen Kelly <steveire@gmail.com>
2018-04-24 22:55:47 +0100

    Use rvalue refs with clang-cl

--- include/rapidjson/rapidjson.h
+++ include/rapidjson/rapidjson.h
@@ -543,7 +543,7 @@ RAPIDJSON_NAMESPACE_END
 #ifndef RAPIDJSON_HAS_CXX11_RVALUE_REFS
 #if defined(__clang__)
 #if __has_feature(cxx_rvalue_references) && \
-    (defined(_LIBCPP_VERSION) || defined(__GLIBCXX__) && __GLIBCXX__ >= 20080306)
+    (defined(_MSC_VER) || defined(_LIBCPP_VERSION) || defined(__GLIBCXX__) && __GLIBCXX__ >= 20080306)
 #define RAPIDJSON_HAS_CXX11_RVALUE_REFS 1
 #else
 #define RAPIDJSON_HAS_CXX11_RVALUE_REFS 0
