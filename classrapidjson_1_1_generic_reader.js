2ff12e00c7947e7669bd62ee0affbec41908f06f
miloyip <miloyip@gmail.com>
2018-05-04 02:29:29 +0000

    Automatic doxygen build

--- classrapidjson_1_1_generic_reader.js
+++ classrapidjson_1_1_generic_reader.js
@@ -5,7 +5,7 @@ var classrapidjson_1_1_generic_reader =
     [ "GetErrorOffset", "classrapidjson_1_1_generic_reader.html#ab50019e0a715320f83b7610b83dcef8f", null ],
     [ "GetParseErrorCode", "classrapidjson_1_1_generic_reader.html#a042c621cf745c5ed3a6f5ff9418dd05e", null ],
     [ "HasParseError", "classrapidjson_1_1_generic_reader.html#ac42370c3497a0e2b6973110f298e3a59", null ],
-    [ "IterativeParseComplete", "classrapidjson_1_1_generic_reader.html#a60fa11ff60f71b3a466bbd962f35dfdb", null ],
+    [ "IterativeParseComplete", "classrapidjson_1_1_generic_reader.html#a037aee35044b894d1487b42365aadc4b", null ],
     [ "IterativeParseInit", "classrapidjson_1_1_generic_reader.html#a90403a2c55dadf82fd2bb9d4c66b3280", null ],
     [ "IterativeParseNext", "classrapidjson_1_1_generic_reader.html#abac6611bec17167de50fede5cd5194ca", null ],
     [ "Parse", "classrapidjson_1_1_generic_reader.html#ac9c540b77de19661f6f45e04b9b0937b", null ],
--- zh-cn/search/functions_8.js
+++ zh-cn/search/functions_8.js
@@ -4,7 +4,7 @@ var searchData=
   ['iscomplete',['IsComplete',['../classrapidjson_1_1_writer.html#a190d964d3e62155e87153e858a0b04da',1,'rapidjson::Writer']]],
   ['iserror',['IsError',['../group___r_a_p_i_d_j_s_o_n___e_r_r_o_r_s.html#ab6d87c6ce9c849f35e902fbe23f485c4',1,'rapidjson::ParseResult']]],
   ['isvalid',['IsValid',['../classrapidjson_1_1_generic_pointer.html#a98b96d54c2c3378f1892ffffc501069c',1,'rapidjson::GenericPointer::IsValid()'],['../classrapidjson_1_1_generic_schema_validator.html#ad32a9ad31dbb588edaaf038ab053275b',1,'rapidjson::GenericSchemaValidator::IsValid()']]],
-  ['iterativeparsecomplete',['IterativeParseComplete',['../classrapidjson_1_1_generic_reader.html#a60fa11ff60f71b3a466bbd962f35dfdb',1,'rapidjson::GenericReader']]],
+  ['iterativeparsecomplete',['IterativeParseComplete',['../classrapidjson_1_1_generic_reader.html#a037aee35044b894d1487b42365aadc4b',1,'rapidjson::GenericReader']]],
   ['iterativeparseinit',['IterativeParseInit',['../classrapidjson_1_1_generic_reader.html#a90403a2c55dadf82fd2bb9d4c66b3280',1,'rapidjson::GenericReader']]],
   ['iterativeparsenext',['IterativeParseNext',['../classrapidjson_1_1_generic_reader.html#abac6611bec17167de50fede5cd5194ca',1,'rapidjson::GenericReader']]]
 ];
