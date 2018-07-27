748a652f04cd3a202ce3639770238bd9473b300c
Veselin Georgiev <veselin.georgiev@garmin.com>
2018-07-27 13:33:09 -0500

    Fix SIGBUS due to unaligned access
    Update RAPIDJSON_ALIGN() to always align on an 8-byte boundary
    unless otherwise overridden.
    
    On some platforms (such as ARM), 64-bit items (such as doubles and
    64-bit integers) must be aligned to an 8 byte address, even though the
    architecture is only 32-bits. On these platforms, MemoryPoolAllocator
    must match the malloc() behavior and return a 8 byte aligned allocation.
    This eliminates any alignment issues that may occur at the expense of
    additional memory overhead.
    
    Failure to do so caused a SIGBUS signal when calling
    GenericValue::SetNull(). The size of the data_ member of the
    GenericValue class is 16 bytes in 32-bit mode and its constructor
    requires an 8-byte aligned access.
    
    While parsing a JSON formatted string using Document::ParseStream(), a
    stack object containing GenericValue items was constructed. Since the
    stack was 8-byte aligned, the constructor calls would succeed. When the
    lifetime of the object ends, SetObjectRaw() is invoked. This triggered
    an allocation with 4-byte alignment to which the previously 8-byte
    aligned GenericValue array was copied. After this, any call to a
    GenericValue API that triggered the constructor and thus the placement
    new operation on the Data type member would trigger a SIGBUS.
    
    Signed-off-by: Veselin Georgiev <veselin.georgiev@garmin.com>
    Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>

--- include/rapidjson/rapidjson.h
+++ include/rapidjson/rapidjson.h
@@ -269,16 +269,11 @@
 /*! \ingroup RAPIDJSON_CONFIG
     \param x pointer to align
 
-    Some machines require strict data alignment. Currently the default uses 4 bytes
-    alignment on 32-bit platforms and 8 bytes alignment for 64-bit platforms.
+    Some machines require strict data alignment. The default is 8 bytes.
     User can customize by defining the RAPIDJSON_ALIGN function macro.
 */
 #ifndef RAPIDJSON_ALIGN
-#if RAPIDJSON_64BIT == 1
-#define RAPIDJSON_ALIGN(x) (((x) + static_cast<uint64_t>(7u)) & ~static_cast<uint64_t>(7u))
-#else
-#define RAPIDJSON_ALIGN(x) (((x) + 3u) & ~3u)
-#endif
+#define RAPIDJSON_ALIGN(x) (((x) + static_cast<size_t>(7u)) & ~static_cast<size_t>(7u))
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
