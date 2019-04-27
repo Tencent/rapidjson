c033292aeaf014accfb5b4054d8fdac36358266c
ylavic <ylavic.dev@gmail.com>
2019-04-27 03:41:19 +0200

    Safer GenericValue& operator=(GenericValue& rhs).
    When rhs is a sub-Value of *this, destroying *this also destroys/frees
    rhs, thus the following RawAssign(rhs) crashes.
    
    Address this by saving/moving rhs to a temporary first, which clears rhs
    and avoids its destruction with *this.
    
    The crash can be reproduced in test Value.MergeDuplicateKey by using the
    CrtAllocator instead of the default Document's MemoryPoolAllocator.

--- include/rapidjson/document.h
+++ include/rapidjson/document.h
@@ -916,8 +916,13 @@ public:
     */
     GenericValue& operator=(GenericValue& rhs) RAPIDJSON_NOEXCEPT {
         if (RAPIDJSON_LIKELY(this != &rhs)) {
+            // Can't destroy "this" before assigning "rhs", otherwise "rhs"
+            // could be used after free if it's an sub-Value of "this",
+            // hence the temporary danse.
+            GenericValue temp;
+            temp.RawAssign(rhs);
             this->~GenericValue();
-            RawAssign(rhs);
+            RawAssign(temp);
         }
         return *this;
     }
