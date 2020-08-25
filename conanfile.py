91b53b3c22928fba67cd3d60ccaf6ca43595fabb
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-25 11:39:59 +0200

    Added conan receipe for rapidjson

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, tools
+import os
+
+
+class RapidjsonConan(ConanFile):
+    name = "rapidjson"
+    version = "1.1.0"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+    homepage = "http://rapidjson.org/"
+    url = "https://github.com/systelab/rapidjson"
+    author = "CSW <csw@werfen.com>"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    no_copy_source = True
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        source_url = "https://github.com/Tencent/rapidjson"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy(pattern="license.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*", dst="include", src=include_folder)
+
+    def package_id(self):
+        self.info.header_only()
