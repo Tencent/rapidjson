15ff9ced864b69c25c278392b1159550cd220eb9
ClausKlein <claus.klein@arcormail.de>
2020-12-21 00:13:21 +0100

    cleanup cmake files
    cmakelint all CMakeLists.txt

---
+++ CMakeModules/before_project_setup.cmake
@@ -0,0 +1,20 @@
+# ===========================================================================
+# CMAKE PART: BUILD-OPTIONS: Setup CMAKE Build Config
+# ===========================================================================
+# USE: INCLUDE-BEFOR project() !!!
+
+include_guard(DIRECTORY)
+
+# ---------------------------------------------------------------------------
+# use ccache if found
+# ---------------------------------------------------------------------------
+find_program(CCACHE_EXECUTABLE "ccache" HINTS /usr/local/bin /opt/local/bin)
+if(CCACHE_EXECUTABLE)
+    message(STATUS "use ccache")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "ccache" FORCE)
+    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "ccache" FORCE)
+endif()
+
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
