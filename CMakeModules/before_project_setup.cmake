059dc731ba32e0a1ea5b6a4d5dfc9be4b60b6943
ClausKlein <claus.klein@arcormail.de>
2020-12-21 18:16:00 +0100

    respect review comments

--- CMakeModules/before_project_setup.cmake
+++ CMakeModules/before_project_setup.cmake
@@ -8,13 +8,11 @@ include_guard(DIRECTORY)
 # ---------------------------------------------------------------------------
 # use ccache if found
 # ---------------------------------------------------------------------------
-find_program(CCACHE_EXECUTABLE "ccache" HINTS /usr/local/bin /opt/local/bin)
+find_program(CCACHE_EXECUTABLE "ccache")
 if(CCACHE_EXECUTABLE)
     message(STATUS "use ccache")
     set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "ccache" FORCE)
     set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "ccache" FORCE)
 endif()
 
-
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
