a585421f98ab0e2fc705fc3d6fba7af39f74bd4b
ClausKlein <claus.klein@arcormail.de>
2020-12-22 15:10:30 +0100

    respect the realy historiy cmake versions too
    VERSION_LESS only usable!

--- CMakeModules/before_project_setup.cmake
+++ CMakeModules/before_project_setup.cmake
@@ -3,18 +3,15 @@
 # ===========================================================================
 # USE: INCLUDE-BEFOR project() !!!
 
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.10)
-    include_guard(DIRECTORY)
-endif()
-
 # ---------------------------------------------------------------------------
 # use ccache if found
 # ---------------------------------------------------------------------------
-find_program(CCACHE_EXECUTABLE "ccache")
-if(CCACHE_EXECUTABLE)
-    message(STATUS "use ccache")
-    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "path to ccache" FORCE)
-    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "path to ccache" FORCE)
+if(NOT DEFINED CMAKE_CXX_COMPILER_LAUNCHER)
+    find_program(CCACHE_EXECUTABLE "ccache")
+    if(CCACHE_EXECUTABLE)
+        message(STATUS "use ccache")
+        set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}" CACHE PATH "path to ccache" FORCE)
+    endif()
 endif()
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
