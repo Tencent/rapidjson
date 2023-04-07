949c771b03de448bdedea80c44a4a5f65284bfeb
Flaviu_ <37621568+flaviu22@users.noreply.github.com>
2023-04-07 12:11:19 +0200

    Resolve conflict with Windows header about max macro

--- include/rapidjson/allocators.h
+++ include/rapidjson/allocators.h
@@ -434,7 +434,7 @@ namespace internal {
 template<typename T, typename A>
 inline T* Realloc(A& a, T* old_p, size_t old_n, size_t new_n)
 {
-    RAPIDJSON_NOEXCEPT_ASSERT(old_n <= std::numeric_limits<size_t>::max() / sizeof(T) && new_n <= std::numeric_limits<size_t>::max() / sizeof(T));
+    RAPIDJSON_NOEXCEPT_ASSERT(old_n <= (std::numeric_limits<size_t>::max)() / sizeof(T) && new_n <= (std::numeric_limits<size_t>::max)() / sizeof(T));
     return static_cast<T*>(a.Realloc(old_p, old_n * sizeof(T), new_n * sizeof(T)));
 }
 
